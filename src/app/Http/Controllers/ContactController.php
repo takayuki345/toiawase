39b3918be08f916b5cf0a1a3e5eed505b1a28ac0
takayuki345 <dom54tak84@gmail.com>
2024-12-29 14:23:36 +0900

    管理画面での「検索」「リセット」ボタンを機能改善

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -14,8 +14,15 @@ class ContactController extends Controller
         $contacts = Contact::with(['category', 'gender'])->paginate(7);
         $categories = Category::all();
         $genders = Gender::all();
+        $searches = [
+            'keyword' => null,
+            'gender_id' => null,
+            'category_id' => null,
+            'date' => null
+        ];
 
-        return view('admin', compact('contacts', 'categories', 'genders'));
+
+        return view('admin', compact('contacts', 'categories', 'genders', 'searches'));
     }
 
     public function search(Request $request)
@@ -23,7 +30,13 @@ class ContactController extends Controller
         $contacts = Contact::with(['category', 'gender'])->KeywordSearch($request->keyword)->GenderSearch($request->gender_id)->CategorySearch($request->category_id)->DateSearch($request->date)->paginate(7);
         $categories = Category::all();
         $genders = Gender::all();
+        $searches = [
+            'keyword' => $request->keyword,
+            'gender_id' => $request->gender_id,
+            'category_id' => $request->category_id,
+            'date' => $request->date
+        ];
 
-        return view('admin', compact('contacts', 'categories', 'genders'));
+        return view('admin', compact('contacts', 'categories', 'genders', 'searches'));
     }
 }
--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -11,22 +11,22 @@
     </div>
     <div class="search-form">
         <form class="search-form__inner">
-            <input class="search-form__item-input" type="text" name="keyword" placeholder="名前やメールアドレスを入力してください" />
+            <input class="search-form__item-input" type="text" name="keyword" placeholder="名前やメールアドレスを入力してください" value="{{ $searches['keyword'] }}" />
             <select class="search-form__item-select" name="gender_id">
                 <option value="">性別</option>
                 <hr>
                 @foreach ($genders as $gender)
-                <option value="{{ $gender['id'] }}">{{ $gender['name'] }}</option>
+                <option value="{{ $gender['id'] }}" @if($gender['id'] == $searches['gender_id']) selected @endif>{{ $gender['name'] }}</option>
                 @endforeach
             </select>
             <select class="search-form__item-select" name="category_id">
                 <option value="">お問い合わせの種類</option>
                 <hr>
                 @foreach ($categories as $category)
-                <option value="{{ $category['id'] }}">{{ $category['content'] }}</option>
+                <option value="{{ $category['id'] }}" @if($category['id'] == $searches['category_id']) selected @endif>{{ $category['content'] }}</option>
                 @endforeach
             </select>
-            <input class="search-form__item-input" type="date" name="date" />
+            <input class="search-form__item-input" type="date" name="date" value="{{ $searches['date'] }}" />
             <button class="search-form__button-search" type="submit" formaction="/admin/search" formmethod="get">検索</button>
             <button class="search-form__button-reset" type="submit" formaction="/admin" formmethod="get">リセット</button>
         </form>
